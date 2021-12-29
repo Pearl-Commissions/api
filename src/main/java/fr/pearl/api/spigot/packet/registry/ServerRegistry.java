0731e4a5af8c5efbcdf6c311abb5d198e5ecc574
Ibramsou <kasobanboui@gmail.com>
2021-12-29 11:39:49 +0100

    Add Packet Scoreboard Display Objective

--- src/main/java/fr/pearl/api/spigot/packet/registry/ServerRegistry.java
+++ src/main/java/fr/pearl/api/spigot/packet/registry/ServerRegistry.java
@@ -3,12 +3,14 @@ package fr.pearl.api.spigot.packet.registry;
 import fr.pearl.api.spigot.PearlSpigot;
 import fr.pearl.api.spigot.packet.PacketServer;
 import fr.pearl.api.spigot.packet.registry.outbound.NmsPacketServerPlayerInfo;
+import fr.pearl.api.spigot.packet.registry.outbound.NmsPacketServerScoreboardDisplayObjective;
 import fr.pearl.api.spigot.packet.registry.outbound.NmsPacketServerScoreboardObjective;
 import fr.pearl.api.spigot.packet.registry.outbound.NmsPacketServerScoreboardTeam;
 
 public enum ServerRegistry {
     PLAYER_INFO(NmsPacketServerPlayerInfo.class),
     SCOREBOARD_OBJECTIVE(NmsPacketServerScoreboardObjective.class),
+    SCOREBOARD_DISPLAY_OBJECTIVE(NmsPacketServerScoreboardDisplayObjective.class),
     SCOREBOARD_TEAM(NmsPacketServerScoreboardTeam.class);
 
     private final Class<? extends PacketServer> packetClass;
---
+++ src/main/java/fr/pearl/api/spigot/packet/registry/outbound/NmsPacketServerScoreboardDisplayObjective.java
@@ -0,0 +1,11 @@
+package fr.pearl.api.spigot.packet.registry.outbound;
+
+import fr.pearl.api.spigot.nms.scoreboard.NmsObjective;
+import fr.pearl.api.spigot.packet.PacketServer;
+
+public interface NmsPacketServerScoreboardDisplayObjective extends PacketServer {
+
+    void setObjective(NmsObjective<?> objective);
+
+    void setType(int type);
+}
