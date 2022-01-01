87a226f965a03520c5faec94235ca0a33e96beb5
Ibramsou <kasobanboui@gmail.com>
2022-01-01 11:40:37 +0100

    Add dynamic config

---
+++ src/main/java/fr/pearl/api/common/configuration/ConfigurationPart.java
@@ -0,0 +1,14 @@
+package fr.pearl.api.common.configuration;
+
+public abstract class ConfigurationPart {
+
+    private PearlConfiguration section;
+
+    public PearlConfiguration getSection() {
+        return section;
+    }
+
+    public void setSection(PearlConfiguration section) {
+        this.section = section;
+    }
+}
--- src/main/java/fr/pearl/api/common/configuration/ConfigurationPath.java
+++ src/main/java/fr/pearl/api/common/configuration/ConfigurationPath.java
@@ -10,4 +10,6 @@ import java.lang.annotation.Target;
 public @interface ConfigurationPath {
 
     String value();
+
+    String[] comments() default {};
 }
