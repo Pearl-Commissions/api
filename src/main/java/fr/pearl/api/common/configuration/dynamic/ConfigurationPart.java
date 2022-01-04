0ca978023cdf7d4d43bd89d4e1d9805dfcb4becc
Ibramsou <kasobanboui@gmail.com>
2022-01-04 23:13:50 +0100

    Remove ConfigurationKey

--- src/main/java/fr/pearl/api/common/configuration/dynamic/ConfigurationPart.java
+++ src/main/java/fr/pearl/api/common/configuration/dynamic/ConfigurationPart.java
@@ -2,5 +2,15 @@ package fr.pearl.api.common.configuration.dynamic;
 
 public abstract class ConfigurationPart {
 
+    private String name;
+
     public void loaded() {}
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
