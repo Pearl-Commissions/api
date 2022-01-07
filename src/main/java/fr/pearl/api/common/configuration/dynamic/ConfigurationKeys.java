baaaf6e55e4059335d2cb6a5e432ca46dffbc32f
Ibramsou <kasobanboui@gmail.com>
2022-01-07 19:53:40 +0100

    Remove comment type

--- src/main/java/fr/pearl/api/common/configuration/dynamic/ConfigurationKeys.java
+++ src/main/java/fr/pearl/api/common/configuration/dynamic/ConfigurationKeys.java
@@ -1,7 +1,5 @@
 package fr.pearl.api.common.configuration.dynamic;
 
-import org.simpleyaml.configuration.comments.CommentType;
-
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
--- src/main/java/fr/pearl/api/common/configuration/dynamic/ConfigurationPath.java
+++ src/main/java/fr/pearl/api/common/configuration/dynamic/ConfigurationPath.java
@@ -1,7 +1,5 @@
 package fr.pearl.api.common.configuration.dynamic;
 
-import org.simpleyaml.configuration.comments.CommentType;
-
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -16,6 +14,4 @@ public @interface ConfigurationPath {
     String[] comments() default {};
 
     String commentPath() default "";
-
-    CommentType commentType() default CommentType.BLOCK;
 }
