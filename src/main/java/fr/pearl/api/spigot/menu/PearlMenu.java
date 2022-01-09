235226365c3373800da324c3213cbd4aee23a49d
Ibramsou <kasobanboui@gmail.com>
2022-01-09 23:50:23 +0100

    Use menu item in setItem method

--- src/main/java/fr/pearl/api/spigot/menu/PearlMenu.java
+++ src/main/java/fr/pearl/api/spigot/menu/PearlMenu.java
@@ -1,13 +1,12 @@
 package fr.pearl.api.spigot.menu;
 
-import fr.pearl.api.spigot.menu.item.AbstractItem;
 import org.bukkit.entity.Player;
 
 public interface PearlMenu {
 
     void open(Player player);
 
-    void setItem(int slot, AbstractItem item);
+    void setItem(int slot, MenuItem item);
 
     void refreshSlots();
 
