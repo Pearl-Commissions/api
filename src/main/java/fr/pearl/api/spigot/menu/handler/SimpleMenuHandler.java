32dc730ef001cf8f882dc4e015f98f4a29207515
Ibramsou <kasobanboui@gmail.com>
2022-01-11 15:06:31 +0100

    Fix menuhandler

--- src/main/java/fr/pearl/api/spigot/menu/handler/SimpleMenuHandler.java
+++ src/main/java/fr/pearl/api/spigot/menu/handler/SimpleMenuHandler.java
@@ -2,6 +2,7 @@ package fr.pearl.api.spigot.menu.handler;
 
 import fr.pearl.api.spigot.menu.MenuItem;
 import fr.pearl.api.spigot.menu.PearlMenu;
+import fr.pearl.api.spigot.menu.type.SimplePearlMenu;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryType;
 
@@ -21,5 +22,5 @@ public interface SimpleMenuHandler {
         return 9;
     }
 
-    default void onOpen(PearlMenu menu, Player player) {}
+    default void onOpen(SimplePearlMenu menu, Player player) {}
 }
